f8d82df47ae53394b3412e7b740fdbd01d05fb86
zuberypim <zuberypima12@gmail.com>
2023-03-27 17:36:12 +0300

    update screens

--- lib/screens/authscreens/farmerregister.dart
+++ lib/screens/authscreens/farmerregister.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:farmsoko/screens/select_crop_page.dart';
 import 'package:farmsoko/utilities/constantscolors.dart';
 import 'package:flutter/material.dart';
 
@@ -15,23 +16,29 @@ class FarmerRegPage extends StatelessWidget {
         child: ListView(
           children: [
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10,0),
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('First Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'First Name',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: 'First Name',
                       ),
                     ),
@@ -40,23 +47,29 @@ class FarmerRegPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10,0),
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('Last Name',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'Last Name',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: 'Last Name',
                       ),
                     ),
@@ -65,23 +78,29 @@ class FarmerRegPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10,0),
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('Email Address',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'Email Address',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: 'Emaail',
                       ),
                     ),
@@ -90,23 +109,29 @@ class FarmerRegPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10,0),
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('Phone Number',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: '+255 67 890 786',
                       ),
                     ),
@@ -115,79 +140,93 @@ class FarmerRegPage extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10,0),
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('Wilaya',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'Wilaya',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: 'Emaail',
                       ),
                     ),
                   ),
-                  
                 ],
               ),
             ),
-             Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10,0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(2, 0, 0,5),
-                    child: Text('Mkoa',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,),),
+                    padding: const EdgeInsets.fromLTRB(2, 0, 0, 5),
+                    child: Text(
+                      'Mkoa',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
                   Container(
                     height: 45,
                     color: Colors.white,
                     child: TextFormField(
                       decoration: InputDecoration(
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
-                        focusedBorder:
-                            OutlineInputBorder(borderSide: BorderSide(width: 2)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 2)),
                         hintText: 'eg.Tanga',
                       ),
                     ),
                   ),
-                  
                 ],
               ),
             ),
-
-             Padding(
-               padding: const EdgeInsets.fromLTRB(10, 30, 0, 0),
-               child: Center(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 30, 0, 0),
+              child: Center(
+                  child: InkWell(
+                onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>CropTypePage()));
+                },
                 child: Container(
-                         width: 130,
-                         height: 40,
-                         decoration: BoxDecoration(
-                  color: Colors.cyan,
-                  border: Border.all(color: Colors.grey, width: 2),
-                  borderRadius: BorderRadius.circular(10)),
-                         child: Center(
-                child: Text(
-                  'Sajili',
-                  style: TextStyle(
-                      fontSize: ConstantsColors().textSizeOne,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
+                  width: 130,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      border: Border.all(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: Text(
+                      'Sajili',
+                      style: TextStyle(
+                          fontSize: ConstantsColors().textSizeOne,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
                 ),
-                         ),
-                       )),
-             ),
+              )),
+            ),
           ],
         ),
       ),
--- lib/screens/select_crop_page.dart
+++ lib/screens/select_crop_page.dart
@@ -1,5 +1,5 @@
+import 'package:farmsoko/utilities/constantscolors.dart';
 import 'package:flutter/material.dart';
-
 
 class CropTypePage extends StatefulWidget {
   const CropTypePage({super.key});
@@ -11,6 +11,18 @@ class CropTypePage extends StatefulWidget {
 class _CropTypePageState extends State<CropTypePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: ConstantsColors().mainColor(),
+        title: Text('Chagua Mazao')),
+      body: Column(
+        children: [
+          Container(
+            child: Text('Chagua zao ambalo ungependa kulilima mimu huu'),
+          )
+        ],
+      ),
+    );
   }
 }
