8ceed9a5a9c8079ba061d5724e981babe05884ce
zuberypim <zuberypima12@gmail.com>
2023-03-29 22:05:50 +0300

    update screens

--- lib/screens/select_crop_page.dart
+++ lib/screens/select_crop_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers, sized_box_for_whitespace
+
 import 'package:farmsoko/utilities/constantscolors.dart';
 import 'package:flutter/material.dart';
 
@@ -13,13 +15,36 @@ class _CropTypePageState extends State<CropTypePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: ConstantsColors().mainColor(),
-        title: Text('Chagua Mazao')),
-      body: Column(
+          elevation: 0,
+          backgroundColor: ConstantsColors().mainColor(),
+          title: Text('Chagua Mazao')),
+      body: ListView(
         children: [
           Container(
-            child: Text('Chagua zao ambalo ungependa kulilima mimu huu'),
+            height: 100,
+            
+            decoration: BoxDecoration(
+                border: Border.all(width: 3, color: Colors.grey,),
+                 borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30))
+                
+                
+                ),
+            child: Text(
+              'Tafadhali chagua Mazao unayojihusishanayo',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: 2,
+              itemBuilder: (BuildContext context, int index) {
+                return Stack(children: [Text('data')],);
+              },
+            ),
           )
         ],
       ),
