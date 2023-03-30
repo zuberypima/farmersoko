fd39a0abce75acc1641f48afec1fb497dd611503
zuberypim <zuberypima12@gmail.com>
2023-03-30 16:04:49 +0300

    update screens

--- lib/screens/select_crop_page.dart
+++ lib/screens/select_crop_page.dart
@@ -14,50 +14,69 @@ class SelecteCropPage extends StatefulWidget {
 class _SelecteCropPageState extends State<SelecteCropPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          elevation: 0,
-          backgroundColor: ConstantsColors().mainColor(),
-          title: Text('Chagua Mazao')),
-      body: ListView(
-        children: [
-          Container(
-            height: 100,
-            decoration: BoxDecoration(
-                border: Border.all(
-                  width: 3,
-                  color: Colors.grey,
-                ),
-                borderRadius:
-                    BorderRadius.only(bottomLeft: Radius.circular(30))),
-            child: Text(
-              'Tafadhali chagua Mazao unayojihusishanayo',
-              style: TextStyle(fontSize: 16),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            elevation: 0,
+            backgroundColor: ConstantsColors().mainColor(),
+            title: Text('Chagua Mazao')),
+        body: ListView(
+          children: [
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 3,
+                    color: Colors.grey,
+                  ),
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(30))),
+              child: Text(
+                'Tafadhali chagua Mazao unayojihusishanayo',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height,
-            child: GridView.count(
-              crossAxisSpacing: 1,
-              mainAxisSpacing: 7,
-              crossAxisCount: 2,
-              children: <Widget>[
-                Card(
-                
-                  child: SizedBox(
-                    height: 30,
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              width: 109,
+              child: GridView.count(
+                crossAxisSpacing: 5,
+                mainAxisSpacing: 2,
+                crossAxisCount: 2,
+                childAspectRatio: 1.5,
+                children: <Widget>[
+                  Card(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Container(),
-                        Container(),
-                        ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            radius: 25,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Mahindi',
+                                style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18),
+                              ),
+                              Icon(Icons.check_circle)
+                            ],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
